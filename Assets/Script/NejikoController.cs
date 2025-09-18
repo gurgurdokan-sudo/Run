d72ab8256356271e86cedebec5d6bd2bef7b4ec8
yuya <gurgur.dokan.gmail>
2025-09-18 19:31:51 +0900

    Projectors Commit

---
+++ Assets/Script/NejikoController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Scripting.APIUpdating;
+using UnityEngine.EventSystems;
+
+public class NejikoController : MonoBehaviour
+{
+    CharacterController controller;
+    Animator animator;
+
+    Vector3 moveDirection;
+
+    public float gravity;
+    public float speedZ;
+    public float speedJump;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            if (Input.GetAxis("Vertical") > 0.0f)
+            {
+                moveDirection.z = Input.GetAxis("Vertical") * speedZ;
+
+            }
+            else
+            {
+                moveDirection.z = 0;
+            }
+            transform.Rotate(0, Input.GetAxis("Horizontal") * 3, 0);
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = speedJump;
+                animator.SetTrigger("jump");
+            }
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        Vector3 globalDirection = transform.TransformDirection(moveDirection);
+        controller.Move(globalDirection * Time.deltaTime);
+        if (controller.isGrounded) moveDirection.y = 0;
+        animator.SetBool("run", moveDirection.x > 0.0f);
+        Debug.Log("test");
+
+    }
+}
